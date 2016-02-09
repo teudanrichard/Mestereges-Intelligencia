4d972aecbdf3c4fdceba84f42923934e2bb15c29
Teudán Ádám Richárd <teudan.richard@gmail.com>
2016-02-09 14:48:50 +0100

    kis javítás
    Kommentelés mindenhol, kicsi javítás

--- MI/src/mi/MI.java
+++ MI/src/mi/MI.java
@@ -28,12 +28,16 @@ public class MI {
             int count = 1;
             while ((sor = br.readLine()) != null) {
                 if (count == 1) {
+                    //legelső sor a pálya nagyságát mutatja meg ezért beállítjuk mielőtt elkezdjük beolvasni az adatokat.
                     setMeret(Integer.parseInt(sor));
                 } else {
+                    //ha nem első sorban vagyunk akkor jöhetnek az adatok, ciklus fut pálya nagyságáig
                     for (int i = 0; i < palya.length; i++) {
+                        //beolvassuk az adatot, spaccel elválasztva van megadva a mátrix így nagyon egyszerűen egy sorból betudjuk olvasni az egészet :)
                         palya[count - 2][i] = Integer.parseInt(sor.split(" ")[i]);
                     }
                 }
+                //növeljük a számlálót
                 count++;
             }
             getTombAllapot();
@@ -44,10 +48,12 @@ public class MI {
         }
     }
 
+    //beállítja a pálya méretét
     private static void setMeret(int n) {
         palya = new int[n][n];
     }
 
+    //kiírja a tömb jelenlegi állapotát
     private static void getTombAllapot() {
         for (int i = 0; i < palya.length; i++) {
             for (int j = 0; j < palya.length; j++) {
@@ -59,37 +65,55 @@ public class MI {
     }
 
     private static void Keres(int x, int y) {
+        //rajta állunk már a kijáraton?
         if (palya[x][y] != 5) {
+            // ha nem akkor megnézzük felfelé,hogy nem-e a pálya tetején vagyunk, ha nem akkor megnézzük,hogy 0-e a felette lévő értéke tehát akkor mehetünk arra....
             if (((x - 1) >= 0) && (palya[x - 1][y] == 0 || palya[x - 1][y] == 5)) {
+                //2-est rakunk mert itt már jártunk :)
                 palya[x][y] = 2; 
+                //újra keresünk csak már a következő lépéstől.
                 Keres(x - 1, y);
             }
+            // megnézzük jobbra,hogy nem-e a pálya szélén vagyunk, ha nem akkor megnézzük,hogy 0-e a mellette lévő értéke tehát akkor mehetünk arra....
             if (((y + 1) < palya.length) && (palya[x][y + 1] == 0 || palya[x][y + 1] == 5)) {
+                //2-est rakunk mert itt már jártunk :)
                 palya[x][y] = 2; 
+                //újra keresünk csak már a következő lépéstől.
                 Keres(x, y + 1);
             }
+            // megnézzük balra,hogy nem-e a pálya szélén vagyunk, ha nem akkor megnézzük,hogy 0-e a mellette lévő értéke tehát akkor mehetünk arra....
             if (((y - 1) >= 0) && (palya[x][y - 1] == 0 || palya[x][y - 1] == 5)) {
+                //2-est rakunk mert itt már jártunk :)
                 palya[x][y] = 2; 
+                //újra keresünk csak már a következő lépéstől.
                 Keres(x, y - 1);
             }
+            // megnézzük lefelé,hogy nem-e a pálya alján vagyunk, ha nem akkor megnézzük,hogy 0-e az alatta lévő értéke tehát akkor mehetünk arra....
             if (((x + 1) < palya.length) && (palya[x + 1][y] == 0 || palya[x + 1][y] == 5)) {
+                //2-est rakunk mert itt már jártunk :)
                 palya[x][y] = 2; 
+                //újra keresünk csak már a következő lépéstől.
                 Keres(x + 1, y);
             }
-            palya[x][y] = 0;            
+            //ez zsákutca :'( ezt jelöljük hármassal nehogy visszamenjünk oda :)
+            palya[x][y] = 3;            
         }else{
+            //hupsz sikerült :) megtaláltuk a kijáratot. kiírjuk az állapotot 
             getTombAllapot();
             setVege(x+1,y+1);
         }
     }
 
     private static void setVege(int x, int y) {
-        System.out.println("Vége a játéknak, meglett a kettes: ");
+        System.out.println("Megvan a kijárat: "+(x+1)+","+(y+1));
     }
 
     public static void main(String[] args) {
+        //beolvassuk a fájlt
         feltolt(new File("test.txt"));
+        //pálya kezdés bal felső sarokban
         palya[0][0] = 3;
+        //keresés bal felső sarokban a többit intézi a rekurzió
         Keres(0, 0);
     }
 

3883ab403a9cae4af77a15ec6676addd1c328426
Teudán Ádám Richárd <teudan.richard@gmail.com>
2016-02-09 19:49:23 +0100

    Kisebb módosítások
    optimalizált verzió

--- MI/src/mi/MI.java
+++ MI/src/mi/MI.java
@@ -40,7 +40,7 @@ public class MI {
                 //növeljük a számlálót
                 count++;
             }
-            getTombAllapot();
+            //getTombAllapot();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(MI.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -65,56 +65,75 @@ public class MI {
     }
 
     private static void Keres(int x, int y) {
+
         //rajta állunk már a kijáraton?
         if (palya[x][y] != 5) {
-            // ha nem akkor megnézzük felfelé,hogy nem-e a pálya tetején vagyunk, ha nem akkor megnézzük,hogy 0-e a felette lévő értéke tehát akkor mehetünk arra....
-            if (((x - 1) >= 0) && (palya[x - 1][y] == 0 || palya[x - 1][y] == 5)) {
-                //2-est rakunk mert itt már jártunk :)
-                palya[x][y] = 2; 
-                //újra keresünk csak már a következő lépéstől.
-                Keres(x - 1, y);
-            }
             // megnézzük jobbra,hogy nem-e a pálya szélén vagyunk, ha nem akkor megnézzük,hogy 0-e a mellette lévő értéke tehát akkor mehetünk arra....
             if (((y + 1) < palya.length) && (palya[x][y + 1] == 0 || palya[x][y + 1] == 5)) {
-                //2-est rakunk mert itt már jártunk :)
-                palya[x][y] = 2; 
+                //2-est rakunk mert itt már jártunk
+                palya[x][y] = 2;
                 //újra keresünk csak már a következő lépéstől.
                 Keres(x, y + 1);
             }
             // megnézzük balra,hogy nem-e a pálya szélén vagyunk, ha nem akkor megnézzük,hogy 0-e a mellette lévő értéke tehát akkor mehetünk arra....
             if (((y - 1) >= 0) && (palya[x][y - 1] == 0 || palya[x][y - 1] == 5)) {
-                //2-est rakunk mert itt már jártunk :)
-                palya[x][y] = 2; 
+                //2-est rakunk mert itt már jártunk
+                palya[x][y] = 2;
                 //újra keresünk csak már a következő lépéstől.
                 Keres(x, y - 1);
             }
+            // ha nem akkor megnézzük felfelé,hogy nem-e a pálya tetején vagyunk, ha nem akkor megnézzük,hogy 0-e a felette lévő értéke tehát akkor mehetünk arra....
+            if (((x - 1) >= 0) && (palya[x - 1][y] == 0 || palya[x - 1][y] == 5)) {
+                //2-est rakunk mert itt már jártunk
+                palya[x][y] = 2;
+                //újra keresünk csak már a következő lépéstől.
+                Keres(x - 1, y);
+            }
             // megnézzük lefelé,hogy nem-e a pálya alján vagyunk, ha nem akkor megnézzük,hogy 0-e az alatta lévő értéke tehát akkor mehetünk arra....
             if (((x + 1) < palya.length) && (palya[x + 1][y] == 0 || palya[x + 1][y] == 5)) {
-                //2-est rakunk mert itt már jártunk :)
-                palya[x][y] = 2; 
+                //2-est rakunk mert itt már jártunk
+                palya[x][y] = 2;
                 //újra keresünk csak már a következő lépéstől.
                 Keres(x + 1, y);
             }
-            //ez zsákutca :'( ezt jelöljük hármassal nehogy visszamenjünk oda :)
-            palya[x][y] = 3;            
-        }else{
-            //hupsz sikerült :) megtaláltuk a kijáratot. kiírjuk az állapotot 
+            //ez zsákutca :'(
+            //ezt jelöljük hármassal nehogy visszamenjünk oda
+            palya[x][y] = 3;
+        } else {
+            int k = 0, c = 0;
+            for (int i = 0; i < palya.length; i++) {
+                for (int j = 0; j < palya.length; j++) {
+                    if (palya[i][j] == 2) {
+                        c++;
+                    }
+                }
+            }
+            for (int i = 0; i < palya.length; i++) {
+                for (int j = 0; j < palya.length; j++) {
+                    if (palya[i][j] == 3) {
+                        k++;
+                    }
+                }
+            }
+            //sikerült megtaláltuk a kijáratot. kiírjuk az állapotot 
             getTombAllapot();
-            setVege(x+1,y+1);
+            setVege(x + 1, y + 1, c, k);
         }
     }
 
-    private static void setVege(int x, int y) {
-        System.out.println("Megvan a kijárat: "+(x+1)+","+(y+1));
+    private static void setVege(int x, int y, int lepes, int hibas) {
+        System.out.println("Megvan a kijárat: " + (x + 1) + "," + (y + 1));
+        System.out.println("Lépésszám: " + lepes);
+        System.out.println("Hibasan bejárt terület: " + hibas);
     }
 
     public static void main(String[] args) {
         //beolvassuk a fájlt
         feltolt(new File("test.txt"));
         //pálya kezdés bal felső sarokban
-        palya[0][0] = 3;
+        palya[1][1] = 3;
         //keresés bal felső sarokban a többit intézi a rekurzió
-        Keres(0, 0);
+        Keres(1, 1);
     }
 
 }
